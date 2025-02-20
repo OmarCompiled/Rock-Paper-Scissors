39ee2a353b75f7adcf82bed9ef12d9a6914fbe66
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-02-20 08:04:45 +0200

    Add button colors and text

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
+const display        = document.querySelector("#display");
+const rockButton     = document.querySelector("#rock");
+const paperButton    = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+const buttons = document.querySelectorAll("input");
 
 let getComputerChoice = () => {
     const choices = ["rock", "paper", "scissors"];
@@ -6,13 +12,11 @@ let getComputerChoice = () => {
 }
 
 let getHumanChoice = () => {
-    let humanChoice = prompt("Rock, Paper, Scissors?");
-    return humanChoice.toLowerCase().trim();
+    display.textContent = "Rock, Paper, Scissors?";
 }
 
 let humanScore = 0;
 let computerScore = 0;
-
 
 
 let playRound = (humanChoice, computerChoice) => {
@@ -37,18 +41,18 @@ let playRound = (humanChoice, computerChoice) => {
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-function playGame(humanSelection, computerSelection) {
-    let roundCount = 1;
-    playRound(humanSelection, computerSelection);
-    while(roundCount < 5) {
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        roundCount++;
-    }
-    let winner = humanScore > computerScore ? "You" : humanScore === computerScore ? "Draw" : "The Computer";
-    console.log(`You won ${humanScore}/5 rounds!`);
-    console.log(winner === "Draw" ? "Draw, nobody won!" : `${winner} won the game!`);
-}
+// function playGame(humanSelection, computerSelection) {
+//     let roundCount = 1;
+//     playRound(humanSelection, computerSelection);
+//     while(roundCount < 5) {
+//         humanSelection = getHumanChoice();
+//         computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection);
+//         roundCount++;
+//     }
+//     let winner = humanScore > computerScore ? "You" : humanScore === computerScore ? "Draw" : "The Computer";
+//     console.log(`You won ${humanScore}/5 rounds!`);
+//     console.log(winner === "Draw" ? "Draw, nobody won!" : `${winner} won the game!`);
+// }
 
-playGame();
+// playGame();
