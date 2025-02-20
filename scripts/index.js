741bddc596bb6b7982a6641f765bf1d0a0d172d6
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-02-20 09:39:52 +0200

    Add changes to game logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,40 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("input");
+const display   = document.querySelector("#display");
+const buttons   = document.querySelectorAll("input");
+const indicator = document.querySelector("#indicator");
+
+let roundCount    = 0;
+let humanScore    = 0;
+let computerScore = 0;
+
+let playRound = (humanChoice, computerChoice) => {
+    if(humanChoice === computerChoice) {
+        display.textContent = "Draw!";
+    } else if(humanChoice === "rock" && computerChoice === "scissors"){
+        humanScore++;
+        display.textContent = "You won this round!";
+    } else if(humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        display.textContent = "You won this round!";
+    } else if(humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        display.textContent = "You won this round!";
+    } else {
+        computerScore++;
+        display.textContent = "The computer won this round!";
+    }
+    roundCount++;
+
+    if(roundCount === 5) {
+        let winner = humanScore === computerScore ? "Nobody" : humanScore > computerScore ? "You" : "I";
+        display.style.fontSize = "18px";
+        display.innerHTML = `You scored ${humanScore} & I scored ${computerScore},
+        <br>${winner} won the game!
+        <br> Press any button to play again.`;
+        buttons.forEach(button => button.addEventListener("click", () => {
+            
+        }));
+    }
+}
 
 let getComputerChoice = () => {
     const choices = ["rock", "paper", "scissors"];
@@ -7,44 +42,8 @@ let getComputerChoice = () => {
     return choices[randomNumber];
 }
 
-let humanScore = 0;
-let computerScore = 0;
+buttons.forEach(button => button.addEventListener("click", () => {
+    playRound(button.value, getComputerChoice());
+    indicator.textContent = `${roundCount}/5`;
+}));
 
-
-let playRound = (humanChoice, computerChoice) => {
-    if(humanChoice === computerChoice) {
-        console.log("Draw!");
-    } else if(humanChoice === "rock" && computerChoice === "scissors"){
-        humanScore++;
-        console.log("You won this round!");
-    } else if(humanChoice === "paper" && computerChoice === "rock") {
-        humanScore++;
-        console.log("You won this round!");
-    } else if(humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore++;
-        console.log("You won this round!");
-    } else {
-        computerScore++;
-        console.log("The Computer won this round!");
-    }
-    return;
-}
-
-buttons.forEach(button => button.addEventListener("click", () => playRound(button.value, getComputerChoice())));
-
-// const computerSelection = getComputerChoice();
-
-// function playGame(humanSelection, computerSelection) {
-//     let roundCount = 1;
-//     playRound(humanSelection, computerSelection);
-//     while(roundCount < 5) {
-//         computerSelection = getComputerChoice();
-//         playRound(humanSelection, computerSelection);
-//         roundCount++;
-//     }
-//     let winner = humanScore > computerScore ? "You" : humanScore === computerScore ? "Draw" : "The Computer";
-//     console.log(`You won ${humanScore}/5 rounds!`);
-//     console.log(winner === "Draw" ? "Draw, nobody won!" : `${winner} won the game!`);
-// }
-
-// playGame();
